d62cca379f43fb4e46731834c7a036ea9b681238
André Rodrigues <andrelmarques11@gmail.com>
2023-07-29 16:51:03 -0300

    Create options to choose the game

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,57 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+    println!("-----------------------SATAN GAMES-----------------------:");
+    println!("Choose the game:");
+    println!("1: Guessing Game");
+    println!("2: Pendule");
+
+    let mut game_number = String::new();
+
+    io::stdin()
+        .read_line(&mut game_number)
+        .expect("Failed to read the game number!");
+
+    let game_number = get_game_number(&mut game_number);
+
+    if game_number == 1 {
+        guessing_game();
+    }
+}
+
+
+fn convert_to_number(input_str: &mut String) -> u32{
+
+    loop {
+         match input_str.trim().parse() {
+           Ok(input_str) => break input_str,
+           Err(_) => {
+               println!("Please type a number!");
+               continue;
+           },
+        };
+    }
+}
+
+fn get_game_number(game_number: &mut String) -> u32{
+
+    return loop {
+        let number = convert_to_number(game_number);
+        if number == 1 {
+            break number;
+        }
+        else if number == 2 {
+            break number;
+        }
+        else {
+            println!("Choose between the options above!");
+            continue;
+        };
+    };
+}
+
+fn guessing_game() {
+
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
